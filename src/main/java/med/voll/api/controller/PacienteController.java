4501aa63d9d18e630ddf86f799e18d1e4ceb6972
CamposJayme <jayme.campos17@gmail.com>
2026-06-22 22:27:18 -0300

    Nova migration, criação de repository, classe e tabela de pacientes

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,15 +1,22 @@
 package med.voll.api.controller;
 
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import med.voll.api.cliente.DadosCadastroPaciente;
+import med.voll.api.cliente.Paciente;
+import med.voll.api.cliente.PacienteRepository;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/pacientes")
 public class PacienteController {
 
+    private PacienteRepository repository;
+
     @PostMapping
-    public void cadastar(@RequestBody DadosCadastroPaciente dados) {
-        System.out.println(dados);
+    @Transactional
+    public void cadastar(@RequestBody @Valid DadosCadastroPaciente dados) {
+        repository.save(new Paciente(dados));
     }
 
 }
